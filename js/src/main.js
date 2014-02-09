85aba27a2a9c5d90c8197c7e9caaaffc38a6b1de
cvanderschuere <christopher.vanderschuere@gmail.com>
2014-02-09 21:04:45 +0000

    added some mobile meta tags

--- js/src/main.js
+++ js/src/main.js
@@ -42,4 +42,13 @@ musicBox.
             otherwise({redirectTo: routePrefix + "home"});
 
         //$locationProvider.html5Mode(true);
+
+	// When ready...
+	window.addEventListener("load",function() {
+		// Set a timeout...
+		setTimeout(function(){
+			// Hide the address bar!
+			window.scrollTo(0, 1);
+		}, 0);
 	});
+});
